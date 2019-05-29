362d79042677397b1be719028bd3f29eda257a0b
Marius Ngaboyamahina <ntezi@Marius.local>
2019-05-29 22:23:24 +0900

    optimise the cls

--- extractors/test_hotel_aspects.py
+++ extractors/test_hotel_aspects.py
@@ -1,15 +1,14 @@
 from __future__ import unicode_literals, print_function
 from spacy.lang.en import English  # updated
 
+import pickle
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer, TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
-
 from preprocess import Preprocess, GetFile
-from classify import Classifier
 from test import Test, ModelFiles
 
 from boto_nlp import BotoNLP
+
+model_file = ModelFiles().naive_model_file
 
 
 class TestHotelAspects:
@@ -24,77 +23,55 @@ class TestHotelAspects:
         x_test = test_data['sentences']
         y = 'aspects'
 
-        model_file = ModelFiles().naive_model_file
         results_file = GetFile().hotel_aspects_result_file
 
         Test(y, test_data, x_test, model_file, results_file)
 
 
 class ClassifyHotelAspects:
-    def __init__(self, train_data_file, test_data_file, results_file):
+    def __init__(self, test_data_file):
         encoding = 'latin-1'
-
-        sample = 500
-        # train_data = Preprocess().get_sample_data(train_data_file, sample, encoding)
-        train_data = Preprocess().get_ready_data(train_data_file, encoding, 'train')
-
         sample = 5
-        test_data = Preprocess().get_sample_data(test_data_file, sample, encoding)
-        # test_data = Preprocess().get_ready_data(test_data_file, encoding, 'test')
 
-        x_train = train_data['sentences']
-        y_train = train_data['aspects']
-
-        self.classify(x_train, y_train, test_data, results_file)
+        # test_data = Preprocess().get_sample_data(test_data_file, sample, encoding)
+        test_data = Preprocess().get_ready_data(test_data_file, encoding, 'test')
+        self.get_aspects(test_data)
 
         # aspects_with_sentiment_aws_file = GetFile().aspects_with_sentiment_aws_file
         # self.merge_aspects_and_sentiments_with_reviews(aspects_with_sentiment_aws_file)
 
-    def sentencizer(self, index, doc):
-        print(doc)
-        for sent in doc.sents:
-            sentence = (index, sent.text)
-            print(sentence)
-            return sentence
-
-    def get_aspects(self, test_data, classifier, features, x_train, y_train):
+    def classify_aspects(self, test_data):
         nlp = English()
         nlp.add_pipe(nlp.create_pipe('sentencizer'))
 
         aspects = []
         for index, row in test_data.iterrows():
             doc = nlp(str(row['review']))
+
+            model = pickle.load(open(model_file, 'rb'))
+
             for sent in doc.sents:
                 x_test = [sent.text]
-                y_test = Classifier().fit_test_classifier(classifier, features, x_train, y_train, x_test)
+                y_test = model.predict(x_test)
                 x_test = ', '.join(x_test)
                 y_test = ', '.join(y_test)
                 aspect = (index, x_test, y_test)
                 aspects.append(aspect)
         return aspects
 
-    def classify(self, x_train, y_train, test_data, results_file):
-        classifier = MultinomialNB()
-        features = ('bow', CountVectorizer(analyzer=Preprocess().get_text_tokens))
+    def get_aspects(self, test_data, ):
 
         test_data['index'] = test_data.index
 
         # Save sentences with  their aspects
-        # aspects = self.get_aspects(test_data, classifier, features, x_train, y_train)
-        # df_aspects = pd.DataFrame(aspects, columns=['id', 'sentences', 'aspects'])
-        # df_aspects.to_csv(results_file)
+        aspects = self.classify_aspects(test_data)
+        df_aspects = pd.DataFrame(aspects, columns=['index', 'sentences', 'aspects'])
 
-        # grouped_aspects = df_aspects.groupby('index')['aspects'].apply(lambda x: ';'.join(x.astype(str))).reset_index()
-        # merged_aspects_with_reviews = pd.merge(test_data, grouped_aspects[['index', 'aspects']], on='index')
-        # merged_aspects_with_reviews.to_csv(results_file)
+        grouped_aspects = df_aspects.groupby('index')['aspects'].apply(lambda x: ';'.join(x.astype(str))).reset_index()
+        merged_aspects_with_reviews = pd.merge(test_data, grouped_aspects[['index', 'aspects']], on='index')
 
-        sentences = []
-        for index, row in test_data.iterrows():
-            sentence = self.sentencizer(index, row['review'])
-            sentences.append(sentence)
-
-        df_sentences = pd.DataFrame(sentences, columns=['review_id', 'sentences'])
-        df_sentences.to_csv(results_file)
+        result_merged_with_aspects_file = GetFile().tripadvisor_hotel_reviews_result_merged_with_aspects_file
+        merged_aspects_with_reviews.to_csv(result_merged_with_aspects_file)
 
         # Sentiments analysis with aws boto at sentence level
         # aspects_with_sentiment_aws_file = GetFile().aspects_with_sentiment_aws_file
@@ -114,9 +91,7 @@ class ClassifyHotelAspects:
 
 if __name__ == '__main__':
     hotel_aspects_test_file = GetFile().hotel_aspects_test_file
-    TestHotelAspects(hotel_aspects_test_file)
+    # TestHotelAspects(hotel_aspects_test_file)
 
-    tripadvisor_hotel_reviews_result_merged_with_aspects_file = GetFile().tripadvisor_hotel_reviews_result_merged_with_aspects_file
-    aspects_file = GetFile().aspects_file
     tripadvisor_hotel_reviews_result_file = GetFile().tripadvisor_hotel_reviews_result_file
-    # ClassifyHotelAspects(hotel_aspects_train_file, tripadvisor_hotel_reviews_result_file, aspects_file)
+    ClassifyHotelAspects(tripadvisor_hotel_reviews_result_file)
