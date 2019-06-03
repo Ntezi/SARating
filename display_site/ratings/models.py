e3361f41f65517a172b72e665a1f91d488db35bc
Marius Ngaboyamahina <ntezi@Marius.local>
2019-06-03 11:55:36 +0900

    hotels_aspects_aggregation

--- display_site/ratings/models.py
+++ display_site/ratings/models.py
@@ -11,6 +11,13 @@ class Reviews(models.Model):
     title = models.CharField(max_length=255)
     user = models.CharField(max_length=255)
     stars = models.SmallIntegerField()
+    breakfast_food_drink = models.IntegerField(default=0)
+    comfort_facilities = models.IntegerField(default=0)
+    location = models.IntegerField(default=0)
+    miscellaneous = models.IntegerField(default=0)
+    overall = models.IntegerField(default=0)
+    service_staff = models.IntegerField(default=0)
+    value_for_money = models.IntegerField(default=0)
 
     class Meta:
         abstract = True
@@ -23,11 +30,18 @@ class Business(models.Model):
     reviews = models.ArrayModelField(
         model_container=Reviews,
     )
-    category = models.CharField(max_length=20)
-    location = models.CharField(max_length=20)
-    address = models.CharField(max_length=20)
-    star_ratings = models.CharField(max_length=5)
-    positive_reviews = models.IntegerField()
-    negative_reviews = models.IntegerField()
+    category = models.CharField(max_length=20, default='NONE')
+    location = models.CharField(max_length=20, default='NONE')
+    address = models.CharField(max_length=20, default='NONE')
+    star_ratings = models.CharField(max_length=5, default='NONE')
+    positive_reviews = models.IntegerField(default=0)
+    negative_reviews = models.IntegerField(default=0)
     total_reviews = models.IntegerField()
     ratings = models.DecimalField(max_digits=5, decimal_places=2)
+    breakfast_food_drink = models.IntegerField(default=0)
+    comfort_facilities = models.IntegerField(default=0)
+    location_aspects = models.IntegerField(default=0)
+    miscellaneous = models.IntegerField(default=0)
+    overall = models.IntegerField(default=0)
+    service_staff = models.IntegerField(default=0)
+    value_for_money = models.IntegerField(default=0)
