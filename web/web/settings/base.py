2b681e6106243a0682a55ab54cb4f0fb445c7741
Marius Ngaboyamahina <ntezi@Marius.local>
2019-07-01 22:13:13 +0900

    production

--- web/web/settings/base.py
+++ web/web/settings/base.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ratings.apps.RatingsConfig',
-    # 'rest_framework',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
