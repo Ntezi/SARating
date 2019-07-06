a92c25fdf4d5ad6b9fa29e4532fb1866e98242d4
Marius Ngaboyamahina <ntezi@Marius.local>
2019-07-06 20:19:14 +0900

    deploy fixes

--- web/web/settings/base.py
+++ web/web/settings/base.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ratings.apps.RatingsConfig',
-    'rest_framework',
+    'ratings.apps.RatingsConfig'
 ]
 
 MIDDLEWARE = [
